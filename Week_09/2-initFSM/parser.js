288b69b0f551c2ec8f7ce4a631b2255ff10cc056
lujiajian1 <lujiajian_123@163.com>
2021-04-21 10:38:24 +0800

    HTML解析 | 用FSM实现HTML的分析

---
+++ Week_09/2-initFSM/parser.js
@@ -0,0 +1,13 @@
+const { symlink } = require("fs/promises");
+
+const EOF = Symbol("EOF");
+
+function data(c) {}
+
+module.exports.parseHTML = function parseHTML(html) {
+  let state = data;
+  for (let c of html) {
+    state = state(c);
+  }
+  state = state(EOF);
+};
