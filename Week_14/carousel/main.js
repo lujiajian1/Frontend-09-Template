e3c4d06f58a7a1bb4462ad732b07152737d9d266
lujiajian <lujiajian_123@163.com>
2021-06-18 22:04:13 +0800

    鼠标滑动手势

--- Week_14/carousel/main.js
+++ Week_14/carousel/main.js
@@ -25,19 +25,34 @@ class Carousel extends Component {
 
       let move = (event) => {
         let x = event.clientX - startX;
-        for (let child of children) {
-          child.style.transition = "none";
-          child.style.transform = `translateX(${-position * 500 + x}px)`;
+
+        let current = position - Math.round((x - (x % 500)) / 500);
+
+        for (let offset of [-1, 0, 1]) {
+          let pos = current + offset;
+          pos = (pos + children.length) % children.length;
+
+          children[pos].style.transition = "none";
+          children[pos].style.transform = `translateX(${
+            -pos * 500 + offset * 500 + (x % 500)
+          }px)`;
         }
       };
 
       let up = (event) => {
         let x = event.clientX - startX;
         position = position - Math.round(x / 500);
-        for (let child of children) {
-          child.style.transition = "";
-          child.style.transform = `translateX(${-position * 500}px)`;
+
+        for (let offset of [0, -Math.sign(Math.round(x / 500) - x + 250 * Math.sign(x))]) {
+          let pos = position + offset;
+          pos = (pos + children.length) % children.length;
+
+          children[pos].style.transition = "";
+          children[pos].style.transform = `translateX(${
+            -pos * 500 + offset * 500
+          }px)`;
         }
+
         document.removeEventListener("mousemove", move);
         document.removeEventListener("mouseup", up);
       };
